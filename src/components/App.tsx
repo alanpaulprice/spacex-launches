96a9aa7948c0fd94716368f7b5635b464383d35d
Alan Price <alan.price@diverseinteractive.com>
2020-12-08 11:53:51 +0000

    App: flesh out layout, including filter/sort buttons, main image & ResultsList

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import Header from './Header';
+import Button from './Button';
+import ResultsList from './ResultsList';
+import LaunchImage from '../assets/images/launch-home.png';
+import SelectIcon from '../assets/icons/select.png';
+import SortIcon from '../assets/icons/sort.png';
 
 export default function App(): JSX.Element {
 	return (
 		<div className="app">
 			<Header />
+			<main className="app__main">
+				<div className="app__filter-sort">
+					<Button onClick={() => null}>
+						Filter by Year
+						<img className="header__logo-image" src={SelectIcon} alt="Select" />
+					</Button>
+					<Button onClick={() => null}>
+						Sort Descending
+						<img className="header__logo-image" src={SortIcon} alt="Sort" />
+					</Button>
+				</div>
+				<div className="app__left-column">
+					<img className="app__main-image" src={LaunchImage} alt="A rocket launch in progress" />
+				</div>
+				<div className="app__right-column">
+					<ResultsList />
+				</div>
+			</main>
 		</div>
 	);
 }
