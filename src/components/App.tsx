29dd2177ee5d8ce2b874c2cff71416f365514993
Alan Price <alan.price@diverseinteractive.com>
2020-12-08 11:38:55 +0000

    App.tsx: add return type

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './Header';
 
-export default function App() {
+export default function App(): JSX.Element {
 	return (
 		<div className="app">
 			<Header />
