d99bbae26b4a651ecf27989f0b4658426fac6701
Alan Price <alan.price@diverseinteractive.com>
2020-12-10 20:18:43 +0000

    App.tsx: implement filter by year functionality

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { fetchLaunches, sortLaunchesByDate } from '../Utilities';
+import { fetchLaunches, sortLaunchesByDate, filterLaunchesByYear, removeArrayDuplicates, sortNumbers } from '../Utilities';
 import Header from './Header';
 import Button from './Button';
+import Select from './Select';
 import ResultsList from './ResultsList';
 import { ILaunchDataItem } from '../interfaces/ILaunchDataItem';
 import LaunchImage from '../assets/images/launch-home.png';
@@ -11,6 +12,7 @@ import SortIcon from '../assets/icons/sort.png';
 export default function App(): JSX.Element {
 	const [launches, setLaunches] = useState<Array<ILaunchDataItem>>([]);
 	const [sortDateDescending, setSortDateDescending] = useState<boolean>(false);
+	const [launchYearFilterParameter, setLaunchYearFilterParameter] = useState<string>('All');
 
 	useEffect(() => {
 		updateLaunches();
@@ -24,25 +26,46 @@ export default function App(): JSX.Element {
 		setSortDateDescending(!sortDateDescending);
 	}
 
+	function updateFilter(option: string): void {
+		setLaunchYearFilterParameter(option);
+	}
+
+	function buildFilterDropdownListOptions(launches: Array<ILaunchDataItem>): Array<string> {
+		const years = launches.map((launch: ILaunchDataItem): number => parseInt(launch.launch_year));
+		const unique: Array<number> = removeArrayDuplicates(years);
+		const sorted: Array<number> = sortNumbers(unique, true);
+		const strings: Array<string> = sorted.map((year: number): string => String(year));
+		strings.unshift('All');
+		return strings;
+	}
+
+	function buildResultsListData(launches: Array<ILaunchDataItem>): Array<ILaunchDataItem> {
+		const filtered = filterLaunchesByYear(launches, launchYearFilterParameter);
+		const sorted = sortLaunchesByDate(filtered, sortDateDescending);
+		return sorted;
+	}
+
 	return (
 		<div className="app">
 			<Header updateLaunches={updateLaunches} />
 			<main className="app__main">
 				<div className="app__filter-sort">
-					<Button onClick={() => null}>
+					<Select
+						dropdownListOptions={buildFilterDropdownListOptions(launches)}
+						onOptionSelect={(option: string) => updateFilter(option)}>
 						Filter by Year
 						<img className="header__logo-image" src={SelectIcon} alt="Select" />
-					</Button>
+					</Select>
 					<Button onClick={toggleSortAscending}>
 						Sort {sortDateDescending ? 'Ascending' : 'Descending'}
 						<img className="header__logo-image" src={SortIcon} alt="Sort" />
 					</Button>
 				</div>
 				<div className="app__left-column">
-					<img className="app__main-image" src={LaunchImage} alt="A rocket launch in progress" />
+					<img className="app__main-image" src={LaunchImage} alt="A rocket launching" />
 				</div>
 				<div className="app__right-column">
-					<ResultsList launches={sortLaunchesByDate(launches, sortDateDescending)} />
+					<ResultsList launches={buildResultsListData(launches)} />
 				</div>
 			</main>
 		</div>
